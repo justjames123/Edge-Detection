c50a18188945cd131422d849b1ac499c1001dd87
justjames123 <158608044+justjames123@users.noreply.github.com>
2024-08-03 19:16:19 -0500

    Update streamlit_edge_detector.py
    Added slider bar for user to select gaussian blur amount

--- streamlit_edge_detector.py
+++ streamlit_edge_detector.py
@@ -6,15 +6,13 @@ import cv2
 import streamlit as st
 import numpy as np
 
-st.title("Awesome Edge Detector")
+st.title("Darien's Edge Detector")
 st_file = st.file_uploader("Upload image for edge detection", type=['jpg', 'jpeg', 'png'])
 
-
-
 # Function to pre-process image input
-def pre_process(image):
+def pre_process(image,ksize):
     img_gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-    img_gaussian5 = cv2.GaussianBlur(img_gray,(5,5),0,0)
+    img_gaussian5 = cv2.GaussianBlur(img_gray,(ksize,ksize),0,0)
     return img_gray, img_gaussian5
 
 # Function to perform canny edge detection
@@ -27,24 +25,28 @@ if st_file is not None:
     # convert uploaded image to a numpy array
     raw_bytes = np.asarray(bytearray(st_file.read()), dtype=np.uint8)
     image = cv2.imdecode(raw_bytes, cv2.IMREAD_COLOR)
+    blur_amt = st.slider("Set Gaussian Blur Amount:",1,51,5,step=2)
 
-    # run function created earlier
-    img_gray, img_gaussian5 = pre_process(image)
+    # run pre_process function created earlier
+    img_gray, img_gaussian5 = pre_process(image,blur_amt)
     
     # Create placeholders to display input and output images.
-    placeholders = st.columns(3)
+    placeholders = st.columns(2)
     # Display Input image in the first placeholder.
-    placeholders[0].image(image, channels='BGR')
     placeholders[0].markdown("Input Image")
+    placeholders[0].image(image, channels='BGR')
+    
 
     # Display Grayscale image in the second placeholder
-    placeholders[1].image(img_gray, channels='GRAY')
     placeholders[1].markdown("Grayscale Image")
+    placeholders[1].image(img_gray, channels='GRAY')
+    
 
     # Display Gaussian Blurred image in the third placeholder
-    placeholders[2].image(img_gaussian5, channels='GRAY')
-    placeholders[2].markdown("Gaussian Blurred Image, 5x5 Kernal")
-
+    st.markdown(f"Gaussian Blurred Image, {blur_amt}x{blur_amt} Kernal")
+    st.image(img_gaussian5, channels='GRAY')
+    
+    # slider to set lower threshold
     lthresh = st.slider("Set Lower Minimum Threshold for Hysteresis",min_value=0,max_value=200,value=50)
 
     st.title("Edge Detection Results:")
@@ -53,11 +55,11 @@ if st_file is not None:
     org_edges, blur_edges = detect_edge(img_gray,lthresh=lthresh)
 
     # show edge detection results
-    results1 = st.columns(2)
+    # results1 = st.columns(2)
 
-    results1[0].markdown(f"Original, {lthresh} Lower Thresh")
-    results1[0].image(org_edges, channels='GRAY')
+    st.markdown(f"Original, {lthresh} Lower Thresh")
+    st.image(org_edges, channels='GRAY')
 
-    results1[1].markdown(f"Gaussian Pre-Processing, {lthresh} Lower Thresh")
-    results1[1].image(blur_edges, channels='GRAY')
+    st.markdown(f"Gaussian Pre-Processing, {lthresh} Lower Thresh")
+    st.image(blur_edges, channels='GRAY')
 
